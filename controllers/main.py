1773c844632329cd5669a13b36ce2321534f45ac
Matthew Ariel <matthew_ariel_wangsit@yahoo.com>
2018-03-12 12:44:39 +0700

    asd

--- controllers/main.py
+++ controllers/main.py
@@ -9,15 +9,15 @@ from odoo.exceptions import ValidationError
 
 class Academy(http.Controller):
 	
-	@http.route('/classes', auth='public')
+	@http.route('/classes', auth='public', methods=['get'])
 	def index(self):
 		mode = http.request.env['training.center.class']
-		classes = mode.search([['state', '=', 'draft']])
+		classes = mode.search([['state', '=', 'open']])
 		return http.request.render('training_center.index', {
 			'classes': classes
 		})
 
-	@http.route('/classes/register/<int:id>', auth='public')
+	@http.route('/classes/register/<int:id>', auth='public', methods=['get'])
 	def form(self, id):
 		mode = http.request.env['training.center.class']
 		classes = mode.search([['state', '=', 'open']])
@@ -25,10 +25,34 @@ class Academy(http.Controller):
 			'classes': classes, 'id': id
 		})
 
-	@http.route('/classes/register', auth='public')
+	@http.route('/classes/register', auth='public', methods=['get'])
 	def form_empty(self):
 		mode = http.request.env['training.center.class']
 		classes = mode.search([['state', '=', 'open']])
 		return http.request.render('training_center.form', {
 			'classes': classes, 'id': -1
+		})
+
+	@http.route('/classes', auth='public',methods=['post'])
+	def insert(self, **kwargs):
+		part = http.request.env['training.center.participant']
+		participant = part.search([['email', '=', kwargs['email']]])
+		ob = {}
+		ob['class_id'] = int(kwargs['course_class_id'])
+		ob['participant_id'] = participant.id
+		if len(participant)==0:
+			obj = {}
+			obj['name'] = kwargs['name']
+			obj['email'] = kwargs['email']
+			obj['address'] = kwargs['address']
+			obj['phone'] = kwargs['phone']
+			obj['birth_date'] = kwargs['birth_date']
+			x = part.create(obj)
+			ob['participant_id'] = x.id
+		cp = http.request.env['training.center.class.participant']
+		cp.create(ob)
+		mode = http.request.env['training.center.class']
+		classes = mode.search([['state', '=', 'open']])
+		return http.request.render('training_center.index', {
+			'classes': classes
 		})
